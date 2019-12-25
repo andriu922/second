0f958c7ce605e2b51d4ede053be14ca539c58683
andriu922 <gladiuka@ukr.net>
2019-12-25 10:25:22 +0200

    redesign

--- resources/views/image/list.blade.php
+++ resources/views/image/list.blade.php
@@ -5,7 +5,7 @@
 
 @foreach($imgs as $i)
 
-<div class='tabloid'>
+<!-- <div class='tabloid'>
     <div class='inner-tabloid'>
     <span class="tabl edit">{{ $i->id }}<button class='manage-bt'>[[E]]</button> </span>
     <span class="tabl descript"><a href="/image-manager/{{ $i->id }}" class='img-mng'>{{ $i->filename }}</a></span>
@@ -18,7 +18,27 @@
     </span>
     </div>
     <div class="prew"><img src="{{ $i->url }}" alt="" class='used-images'></div>
+</div> -->
+
+<div class="card bg-secondary" style="width: 18rem;">
+    <img src="{{ $i->url }}" alt="" class="card-img-top">
+    <div class="card-body">
+        <h5 class="card-title text-center">ID: {{ $i->id }}</h5>
+        <p class="card-text text-center">
+            A way to image:  <br>
+            {{ $i->filename }}
+        </p>
+        <form action="/image-manager/{{ $i->id }}" method="POST" >
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="bg-danger">[[X]]</button>
+        </form><br>
+        <form action="">
+            <button class="bg-success">[[E]]</button>
+        </form>
+    </div>
 </div>
+
 @endforeach
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,70 +24,12 @@
     <link rel="stylesheet" href="{{ asset('css/second-main.css') }}">
 </head>
 <body>
-    <!-- <div id="app">
-        <nav class="navbar navbar-expand-md">
-            <div class="container">
-                
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-                    <ul class="navbar-nav mr-auto">
-                    </ul>
-
-                    
-                    <ul class="navbar-nav ml-auto">
-                    
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link " href="/account" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="nav-item " >
-                                    <a class="" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-<<<<<<< HEAD
-        </div> -->
-        <!-- <div class="header container-fluid bg-dark text-light">
-            <div class="col-12-lg">
-                <ul class="navigation nav nav-tabs ">
-                    <li class="nav-item"><a href="/home" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
-                    <li class="nav-item"><a href="/license" class="nav-link">License</a></li>
-                    <li class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="/image-manager" class="nav-link">Images</a></li>
-                </ul>
-            
-                
-        </div> -->
-        <div class="header">
-            <nav class="navbar navbar-expand-lg bg-dark text-light ">
-            <a href="/home" class="navbar-brand">Laravel Blog</a>
+        <div class="header fixed-top col-12-lg">
+            <nav class="navbar navbar-expand-lg bg-dark ">
+            <a href="/home" class="navbar-brand site-name">Laravel Blog</a>
                 <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+                    <ul class="navbar-nav mr-auto site-nvg">
+                        <li class="nav-item"><a href="/about" class="nav-link ">About</a></li>
                         <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
                         <li class="nav-item"><a href="/license" class="nav-link">License</a></li>
                         <li class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
@@ -135,10 +77,12 @@
                     </form>
                 </div>
             </nav>
-        </div>    
-        <main class="py-4">
+        </div>  
+        <div class="container">  
+        <main class="py-4 content">
             @yield('content')
         </main>
+        </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
