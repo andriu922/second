488e1ff697fbd04957312951f80cd2bd80d3e2b0
andriu922 <gladiuka@ukr.net>
2019-12-28 11:13:58 +0200

    1212

--- resources/views/image/list.blade.php
+++ resources/views/image/list.blade.php
@@ -5,21 +5,6 @@
 
 @foreach($imgs as $i)
 
-<!-- <div class='tabloid'>
-    <div class='inner-tabloid'>
-    <span class="tabl edit">{{ $i->id }}<button class='manage-bt'>[[E]]</button> </span>
-    <span class="tabl descript"><a href="/image-manager/{{ $i->id }}" class='img-mng'>{{ $i->filename }}</a></span>
-    <span class="tabl del">
-        <form action="/image-manager/{{ $i->id }}" method='POST'>
-            @csrf
-            @method('DELETE')
-            <button class='manage-bt' type='submit'>[[X]]</button>
-        </form>
-    </span>
-    </div>
-    <div class="prew"><img src="{{ $i->url }}" alt="" class='used-images'></div>
-</div> -->
-
 <div class="card bg-secondary" style="width: 18rem;">
     <img src="{{ $i->url }}" alt="" class="card-img-top">
     <div class="card-body">
@@ -28,14 +13,16 @@
             A way to image:  <br>
             {{ $i->filename }}
         </p>
-        <form action="/image-manager/{{ $i->id }}" method="POST" >
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="bg-danger">[[X]]</button>
-        </form><br>
-        <form action="">
-            <button class="bg-success">[[E]]</button>
-        </form>
+        <div class="ctrl-area">    
+            <form action="/image-manager/{{ $i->id }}" method="POST" class="ctrl-btn">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="bg-danger ">[[X]]</button>
+            </form><br>
+            <form action="" class="ctrl-btn">
+                <button class="bg-success ">[[E]]</button>
+            </form>
+        </div>
     </div>
 </div>
 
