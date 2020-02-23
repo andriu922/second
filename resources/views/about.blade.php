64ce809ca7d9ce632f7b2785b1c2a289ed483b37
ВашÐandriu922 <gladiuka@ukr.net>
2020-02-23 13:14:01 +0200

    js flying ball

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,5 @@
-@extends('layouts.app')
-
-@section('title','about')
-@section('content')
-<h3 class='about_us'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus eaque 
-    aliquid dolore repellat dolorum, odit eius commodi sint,
-    temporibus accusantium aspernatur quo fugiat mollitia expedita consectetur
-    adipisci nesciunt, fugit eveniet!
+@extends('layouts.app') @section('title','about') @section('content')
+<script src="js/flyingball.js"></script>
+<h3 class='about_us'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus eaque aliquid dolore repellat dolorum, odit eius commodi sint, temporibus accusantium aspernatur quo fugiat mollitia expedita consectetur adipisci nesciunt, fugit eveniet!
 </h3>
-@endsection
+<canvas id="c1" width="300px" height="300px"></canvas> @endsection
